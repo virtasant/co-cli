00797cb4b127b8602081ba2897ed2d67adf2f108
dschwartzi <49711841+dschwartzi@users.noreply.github.com>
2021-08-12 18:16:31 +0300

    Update ddb_stream_parser.py

--- eb-wrapper/ddb_stream_parser.py
+++ eb-wrapper/ddb_stream_parser.py
@@ -41,3 +41,34 @@ def dict_diff(a, b):
     for i in [key for key in a.keys() & b if a[key] != b[key]]:
        d.update({i:b[i]})
     return d if bool(d) else None
+
+###################################################################
+###################################################################
+
+import json
+import decimal
+
+###################################################################
+
+class DecimalEncoder(json.JSONEncoder): 
+    def default(self, obj):
+        if isinstance(obj, decimal.Decimal):
+            return str(obj)
+        return super(DecimalEncoder, self).default(obj)
+        
+    def to_string(self, obj):
+        if isinstance(obj, decimal.Decimal):
+            return str(obj)
+        return obj
+
+class DecimalDecoder(json.JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
+
+    def object_hook(self, obj):
+        if '_type' not in obj:
+            return obj
+        type = obj['_type']
+        if type == 'decimal':
+            return decimal.Decimal(obj['value'])
+        return obj        
