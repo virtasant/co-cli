37f4cd9f702f7aadfbec7dc5b5f724cd87cf21eb
dschwartzi <49711841+dschwartzi@users.noreply.github.com>
2021-08-12 18:19:31 +0300

    Update lambda_function.py

--- eb-wrapper/lambda_function.py
+++ eb-wrapper/lambda_function.py
@@ -1,6 +1,4 @@
-import json
 from customers_ddb_stream_parser import CustomersDDBStreamParser
 
 def lambda_handler(event, context):
-    print(json.dumps(event))
     CustomersDDBStreamParser().process(event)
